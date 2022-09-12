0b18aaa943c6d150f450cc4e9b85dd99031590e0
Over-lord12 <uchimezie12@gmail.com>
2022-09-11 17:26:51 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Entry point void
- * printf - printed stringst
- * return:  printed (successI)
+ * Description: printed stringst
+ * Return: printed (successI)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
