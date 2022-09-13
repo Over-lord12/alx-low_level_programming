862cd162efbe4c570292adc50064920e9e4f063b
Over-lord12 <uchimezie12@gmail.com>
2022-09-12 23:58:57 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 0; q <= 9; q++)
+	{
+		printf("%d", q);
+	}
+	printf("\n");
+	return (0);
+}
