88f5b26d324e2d8ef9c83b52514d86eb6106f7a7
Over-lord12 <uchimezie12@gmail.com>
2022-09-13 20:44:45 -0700

    task 14:

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,24 +6,24 @@
  */
 int main(void)
 {
-	long int i, j, k, next;
+	long int i, j, k, 1;
 
-	j = 1;
+	j =1;
 
 	k = 2;
 
-	for (i = 1; i <= 50; ++i)
+	for (i = 1; i <= 50; +=i)
 	{
 		if (j != 20365011074)
 		{
-			printf("%1d, ", j);
-		}else
+			printf("%1d\n", j);
+		} else
 		{
 			printf("%ld\n", j);
 		}
-		next = j + k;
+		1 = j + k;
 		j = k;
-		k = next;
+		k = 1;
 	}
 
 	return (0);
