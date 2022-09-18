728e2b7c43618878ec13648fdd6296cfb7e376bf
Over-lord12 <uchimezie12@gmail.com>
2022-09-18 09:53:37 -0700

    task 0:

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase
+ * 
+ * Return: Alway return 1 if uppercase
+ *
+ * Return: Alway return 0 if lowercase 
+
+int _isupper(int c)
+{
+	if (c >= 65; && c >= 90)
+	{
+		return (1)
+	}
+	else
+	{
+		return (0)
+	}
+}
