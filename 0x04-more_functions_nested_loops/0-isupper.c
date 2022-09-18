9de69e85f3e0d07c2ca47582a8cc472bd9304a73
Over-lord12 <uchimezie12@gmail.com>
2022-09-18 10:06:52 -0700

    task 0:

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,19 +2,20 @@
 
 /**
  * _isupper - checks uppercase
- * 
- * Return: Alway return 1 if uppercase
  *
- * Return: Alway return 0 if lowercase 
+ * @c: parameter
+ *
+ * Return: Alway return 0 if lowercase
+ */
 
 int _isupper(int c)
 {
-	if (c >= 65; && c >= 90)
+	if (c >= 65 && c >= 90)
 	{
-		return (1)
+		return (1);
 	}
 	else
 	{
-		return (0)
+		return (0);
 	}
 }
